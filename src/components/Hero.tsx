1f69b2654827170bc7e4488380020b00850d25b2
ryanfkhrji <ryanfakhroji09@gmail.com>
2025-03-17 16:11:45 +0700

    update Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 import Profile from "./Profile";
 import Avatar from "../assets/Photo.png";
 import Verse from "./Verse";
-import Icon from "../assets/IconQuran.png";
+import Icon from "../assets/logo.png";
 
 function Hero() {
   return (
@@ -9,7 +9,7 @@ function Hero() {
       <div className="absolute top-0 left-0 bg-[url('/src/assets/HeroBackgroundImage.png')] bg-cover bg-no-repeat h-60 w-full bg-center rounded-b-3xl"></div>
       <div className="relative z-10 px-5 pt-8 flex-col gap-8 flex">
         <Profile greeting="Good Evening" name="John Doe" avatar={Avatar} />
-        <Verse iconQuran={Icon} verse="“He said: ‘Therein you shall live, and therein you shall die, and from it you shall be brought out (i.e. resurrected).’”" surah="Al-A’raaf (7:25)" />
+        <Verse icon={Icon} verse="“He said: ‘Therein you shall live, and therein you shall die, and from it you shall be brought out (i.e. resurrected).’”" surah="Al-A’raaf (7:25)" />
       </div>
     </div>
   );
--- src/components/Verse.tsx
+++ src/components/Verse.tsx
@@ -1,15 +1,15 @@
 type VerseProps = {
   verse: string;
   surah: string;
-  iconQuran: string
+  icon: string
 }
 
-function Verse ({verse, surah}: VerseProps){
+function Verse ({verse, surah, icon}: VerseProps){
   return (
     <div className="bg-[url('/src/assets/VerseBackgroundImage.png')] p-4 rounded-3xl bg-cover bg-no-repeat text-left min-h-[155px] flex flex-col justify-between gap-2">
       <div className="flex flex-col gap-y-2">
         <p className="text-xs text-light flex gap-x-1 md:text-base">
-          <img src={iconQuran} alt="icon-qur'an" />
+          <img src={icon} alt="icon-qur'an" />
           Your daily verse
         </p>
         <p className="font-semibold text-sm text-light md:text-lg">{verse}</p>
